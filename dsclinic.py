57c9a39924092f3a30c3af9d33caea50b752190a
Nedja995 <nedeljkopejasinovic@icloud.com>
2026-02-17 16:54:11 +0100

    analyse with gemini try

--- dsclinic.py
+++ dsclinic.py
@@ -2,6 +2,7 @@ import os
 import pdfplumber
 from fpdf import FPDF
 from utils import BASE_SYNDROMS
+from utils import api_gemini
 
 #import warnings
 #warnings.filterwarnings("ignore")
@@ -103,10 +104,34 @@ def pokreni_analizu():
 
     if os.name == 'nt': os.startfile(OUTPUT_DIR)
 
+def pokreni_analizu_gemini():
+    if not os.path.exists(OUTPUT_DIR): os.makedirs(OUTPUT_DIR)
+    if not os.path.exists(INPUT_DIR): os.makedirs(INPUT_DIR)
+    
+    # PRONALAZAK PDF FAJLOVA U FOLDERU
+    documents_names = [f for f in os.listdir(INPUT_DIR) if f.lower().endswith('.pdf')]
+    documents_filepaths = [os.path.join(INPUT_DIR, f) for f in documents_names]
+
+    try:
+        gemini_client = api_gemini.gemini_client_connect()
+        res = api_gemini.analyze_lab_result_docs(gemini_client, documents_filepaths[0], documents_filepaths[1])
+        print(f"SUCCESS - RESPONSE: {res}")
+    except Exception as e:
+        print(f"ERROR - GOOGLE SERVICE: {str(e)}")
+
+    # ANALIZA TEXTA I NALAZAK PROTOKOLA
+    #protokoli = analyze_content(text, BASE_SYNDROMS.VELIKA_BAZA)
+
+    # GHENERISANJE IZVESTAJA
+    #generate_report_pdf(document, protokoli, OUTPUT_DIR)
+
+    if os.name == 'nt': os.startfile(OUTPUT_DIR)
+
 
 
 def main():
-    pokreni_analizu()
+    # pokreni_analizu()
+    pokreni_analizu_gemini()
 
 if __name__ == "__main__":
     main()
